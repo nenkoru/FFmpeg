8d92025453e1c15ef03009c63c741e06a1bc51e0
Chernenko Ruslan <ractyfree@gmail.com>
2024-05-01 14:07:52 +0300

    avcodec/videotoolbox: av1 decoding not copying the sequence header obu into the bitstream
    Signed-off-by: Chernenko Ruslan <ractyfree@gmail.com>

--- libavcodec/videotoolbox_av1.c
+++ libavcodec/videotoolbox_av1.c
@@ -19,50 +19,90 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "libavformat/avio.h"
-#include "libavformat/avio_internal.h"
-#define CALLED_FROM_AVCODEC 1
-#include "libavformat/av1.c"
-#undef CALLED_FROM_AVCODEC
 
-#include "libavutil/avutil.h"
-#include "libavutil/frame.h"
-#include "libavutil/pixfmt.h"
+#include "libavutil/mem.h"
 
 #include "av1dec.h"
-#include "codec_id.h"
 #include "hwaccel_internal.h"
 #include "internal.h"
 #include "vt_internal.h"
 
-CFDataRef ff_videotoolbox_av1c_extradata_create(AVCodecContext *avctx)
-{
-    AVIOContext *pb;
-    uint8_t *buf;
-    CFDataRef data = NULL;
-    int buf_size = 0;
-    int ret = avio_open_dyn_buf(&pb);
-    if (ret < 0)
-        return NULL;
 
-    ret = ff_isom_write_av1c(pb, avctx->extradata, avctx->extradata_size, 1);
-    if (ret < 0)
-        goto fail;
+CFDataRef ff_videotoolbox_av1c_extradata_create(AVCodecContext *avctx) {
 
-    buf_size = avio_get_dyn_buf(pb, &buf);
-    if (buf_size)
-        data = CFDataCreate(kCFAllocatorDefault, buf, buf_size);
+    AV1DecContext *s = avctx->priv_data;
+    avctx->extradata = av_fast_realloc(avctx->extradata, 
+            &avctx->extradata_size, 
+            s->seq_data_ref->size + 4);
+    avctx->extradata_size = s->seq_data_ref->size + 4;
+    avctx->extradata[0] = 0x81; // version and marker (constant)
+    avctx->extradata[1] = s->raw_seq->seq_profile << 5 | s->raw_seq->seq_level_idx[0];
+    avctx->extradata[2] = s->raw_seq->seq_tier[0] << 7 |
+                        s->raw_seq->color_config.high_bitdepth << 6 | 
+                        s->raw_seq->color_config.twelve_bit << 5 |
+                        s->raw_seq->color_config.mono_chrome << 4 |
+                        s->raw_seq->color_config.subsampling_x << 3 |
+                        s->raw_seq->color_config.subsampling_y << 2 |
+                        s->raw_seq->color_config.chroma_sample_position;
+                                                                    
 
-fail:
-    ffio_free_dyn_buf(&pb);
-
+    if (s->raw_seq->initial_display_delay_present_flag) 
+        avctx->extradata[3] = 0 << 5 | s->raw_seq->initial_display_delay_present_flag << 4 | s->raw_seq->initial_display_delay_minus_1[0];
+    else
+        avctx->extradata[3] = 0x00;
+    memcpy(avctx->extradata + 4, s->seq_data_ref->data, s->seq_data_ref->size);
+    CFDataRef data = CFDataCreate(kCFAllocatorDefault, avctx->extradata, avctx->extradata_size);
     return data;
+};
+
+
+static int videotoolbox_av1_decode_params(AVCodecContext *avctx,
+                                         int header_type,
+                                         const uint8_t *buffer,
+                                         uint32_t size)
+{
+    VTContext *vtctx = avctx->internal->hwaccel_priv_data;
+    void *tmp;
+
+    tmp = av_fast_realloc(vtctx->bitstream,
+                         &vtctx->allocated_size,
+                         size);
+
+    if (!tmp)
+        return AVERROR(ENOMEM);
+
+    vtctx->bitstream = tmp;
+
+    // copy the sequence header obu into the bitstream
+    memcpy(vtctx->bitstream + vtctx->bitstream_size, 
+            buffer, 
+            size);
+
+    vtctx->bitstream_size += size;
+    return 0;
 }
 
 static int videotoolbox_av1_start_frame(AVCodecContext *avctx,
                                         const uint8_t *buffer,
                                         uint32_t size)
 {
+
+    
+    VTContext *vtctx = avctx->internal->hwaccel_priv_data;
+    void *tmp;
+
+    tmp = av_fast_realloc(vtctx->bitstream,
+                         &vtctx->allocated_size,
+                         size);
+
+    if (!tmp)
+        return AVERROR(ENOMEM);
+
+    vtctx->bitstream = tmp;
+
+    // copy the frame data into the bitstream
+    memcpy(vtctx->bitstream + vtctx->bitstream_size, buffer, size);
+    vtctx->bitstream_size += size;
     return 0;
 }
 
@@ -70,17 +110,18 @@ static int videotoolbox_av1_decode_slice(AVCodecContext *avctx,
                                          const uint8_t *buffer,
                                          uint32_t size)
 {
-    VTContext *vtctx = avctx->internal->hwaccel_priv_data;
-
-    return ff_videotoolbox_buffer_copy(vtctx, buffer, size);
+    return 0;
 }
 
 static int videotoolbox_av1_end_frame(AVCodecContext *avctx)
 {
     const AV1DecContext *s = avctx->priv_data;
+    VTContext *vtctx = avctx->internal->hwaccel_priv_data;
     AVFrame *frame = s->cur_frame.f;
 
-    return ff_videotoolbox_common_end_frame(avctx, frame);
+    int ret =  ff_videotoolbox_common_end_frame(avctx, frame);
+    vtctx->bitstream_size = 0;
+    return ret;
 }
 
 const FFHWAccel ff_av1_videotoolbox_hwaccel = {
@@ -89,6 +130,7 @@ const FFHWAccel ff_av1_videotoolbox_hwaccel = {
     .p.id           = AV_CODEC_ID_AV1,
     .p.pix_fmt      = AV_PIX_FMT_VIDEOTOOLBOX,
     .alloc_frame    = ff_videotoolbox_alloc_frame,
+    .decode_params  = videotoolbox_av1_decode_params,
     .start_frame    = videotoolbox_av1_start_frame,
     .decode_slice   = videotoolbox_av1_decode_slice,
     .end_frame      = videotoolbox_av1_end_frame,
